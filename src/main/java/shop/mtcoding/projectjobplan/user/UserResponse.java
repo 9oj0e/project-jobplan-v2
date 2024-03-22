79b0ba2e53f98971a55d2f6842f5f73c8d1fe280
JHJO <war2290@gmail.com>
2024-03-22 09:45:08 +0900

    user/update-form

--- src/main/java/shop/mtcoding/projectjobplan/user/UserResponse.java
+++ src/main/java/shop/mtcoding/projectjobplan/user/UserResponse.java
@@ -2,30 +2,42 @@ package shop.mtcoding.projectjobplan.user;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 public class UserResponse {
 
     @Data
     public static class DTO {
+        // 회원 정보
+        private Integer id;
+        private String username;
         private String password;
-        private Character gender;
+
+        // 개인 정보
+        private String name;
+        private Timestamp birthdate;
+//        private Character gender;
         private String phoneNumber;
         private String address;
         private String email;
 
+        // 회사 정보
         private String employerIdNumber; // 사업자번호
         private String businessName; // 기업이름
 
         public DTO(User user, User sessionUser) {
+            this.id = user.getId();
+            this.username = user.getUsername();
             this.password = user.getPassword();
-            this.gender = user.getGender();
+            this.name = user.getName();
+            this.birthdate = user.getBirthdate();
             this.phoneNumber = user.getPhoneNumber();
-
-            // 세션 유저의 isEmployer 값이 true면 회사
+            this.address = user.getAddress();
+            this.email = user.getEmail();
             if (sessionUser.getIsEmployer()) {
-                this.address = getAddress();
-                this.email = getEmail();
+                this.employerIdNumber = user.getEmployerIdNumber();
+                this.businessName = user.getBusinessName();
             }
         }
     }
-
 }
