ada9c4d2d8f0232fdafde30aea2309a852212b71
9oj0e <war2290@gmail.com>
2024-03-27 16:34:15 +0900

    entity completed

--- src/main/java/shop/mtcoding/projectjobplan/rating/RatingRequest.java
+++ src/main/java/shop/mtcoding/projectjobplan/rating/RatingRequest.java
@@ -1,4 +1,19 @@
 package shop.mtcoding.projectjobplan.rating;
 
+import lombok.Data;
+
 public class RatingRequest {
+
+    @Data
+    public static class rateBoardUser {
+        private Integer raterId;
+        private Integer boardId;
+        private Integer rating;
+    }
+    @Data
+    public static class rateResumeUser {
+        private Integer raterId;
+        private Integer resumeId;
+        private Integer rating;
+    }
 }
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -248,16 +248,17 @@ INSERT INTO apply_tb
 (resume_id,  board_id,  created_at)
 VALUES (5,  5, now());
 
--- -- 평점
--- -- INSERT INTO rating_tb
--- -- (rater_id, subject_id, rate)
--- -- VALUES (1, 6, 5);
--- -- INSERT INTO rating_tb
--- -- (rater_id, subject_id, rate)
--- -- VALUES (1, 6, 4);
--- -- INSERT INTO rating_tb
--- -- (rater_id, subject_id, rate)
--- -- VALUES (1, 6, 3);
+-- 평점
+INSERT INTO rating_tb
+(rater_id, subject_id, rate, created_at)
+VALUES (1, 6, 5, now());
+INSERT INTO rating_tb
+(rater_id, subject_id, rate, created_at)
+VALUES (1, 6, 4, now());
+INSERT INTO rating_tb
+(rater_id, subject_id, rate, created_at)
+VALUES (1, 6, 3, now());
+
 -- -- subscribe_tb (기업이 이력서를 구독)
 -- INSERT INTO subscribe_tb
 -- (resume_id, resume_user_id, board_id, board_user_id, created_at)
