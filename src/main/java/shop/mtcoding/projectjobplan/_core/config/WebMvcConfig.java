40ef09bec9ec56dce713fe67643f6d0d574e9089
yunchocopie <129649095+yunchocopie@users.noreply.github.com>
2024-03-28 11:43:49 +0900

    로그인 인터셉터 (#41)

--- src/main/java/shop/mtcoding/projectjobplan/_core/config/WebMvcConfig.java
+++ src/main/java/shop/mtcoding/projectjobplan/_core/config/WebMvcConfig.java
@@ -11,8 +11,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/board/**", "/user/**")
-                .excludePathPatterns("/board/{id:\\d+}");
+                .addPathPatterns("/users/**", "/boards/**", "/resumes/**")
+                .excludePathPatterns("/boards/{id:\\d+}", "/boards/listings", "/");
 
     }
 }
