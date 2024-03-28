f122f3d2bb3f21afac6654f8cdf601ff22e6ae3e
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-03-28 11:44:05 +0900

    동동적 에러페이지 (#42)

--- src/main/java/shop/mtcoding/projectjobplan/_core/errors/exception/Exception404.java
+++ src/main/java/shop/mtcoding/projectjobplan/_core/errors/exception/Exception404.java
@@ -1,7 +1,6 @@
 package shop.mtcoding.projectjobplan._core.errors.exception;
 
 public class Exception404 extends RuntimeException {
-
     public Exception404(String msg) {
         super(msg);
     }
