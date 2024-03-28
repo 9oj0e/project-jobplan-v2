9b6dc5be9d21649a261d99f8ca4ac00dc003edf7
David Jo <126438339+9oj0e@users.noreply.github.com>
2024-03-28 11:04:18 +0900

    skill setup (#40)

---
+++ src/main/java/shop/mtcoding/projectjobplan/skill/SkillJpaRepository.java
@@ -0,0 +1,6 @@
+package shop.mtcoding.projectjobplan.skill;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillJpaRepository extends JpaRepository<Skill, Integer> {
+}
