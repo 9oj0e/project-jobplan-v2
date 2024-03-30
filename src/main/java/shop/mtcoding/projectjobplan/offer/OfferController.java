c1808a9111fc6c0f12409f1a8df63be419b3c41c
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-03-30 18:35:13 +0900

    제안 (#75)
    * 제안폼으로 이동
    
    * 제안 완료

--- src/main/java/shop/mtcoding/projectjobplan/offer/OfferController.java
+++ src/main/java/shop/mtcoding/projectjobplan/offer/OfferController.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.projectjobplan.user.User;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +22,14 @@ public class OfferController {
         User user = (User) session.getAttribute("sessionUser");
         OfferResponse.OfferFormDTO respDTO = offerService.getBoardAndResume(resumeId, user);
         request.setAttribute("offerForm", respDTO);
+
         return "offer/offer-form";
+    }
+
+    @PostMapping("/resumes/{resumeId}/offer")
+    public String offer(@PathVariable Integer resumeId, OfferRequest.OfferDTO reqDTO) {
+        offerService.createOffer(reqDTO);
+
+        return "redirect:/resumes/" + resumeId;
     }
 }
--- src/main/java/shop/mtcoding/projectjobplan/offer/OfferService.java
+++ src/main/java/shop/mtcoding/projectjobplan/offer/OfferService.java
@@ -1,7 +1,10 @@
 package shop.mtcoding.projectjobplan.offer;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.projectjobplan._core.errors.exception.Exception404;
 import shop.mtcoding.projectjobplan.board.Board;
 import shop.mtcoding.projectjobplan.board.BoardJpaRepository;
@@ -17,14 +20,30 @@ public class OfferService {
     private final OfferJpaRepository offerJpaRepository;
     private final ResumeJpaRepository resumeJpaRepository;
     private final BoardJpaRepository boardJpaRepository;
+    private final HttpSession session;
 
     public OfferResponse.OfferFormDTO getBoardAndResume(int resumeId, User sessionUser) {
+        System.out.println("1. Resume resume = resumeJpaRepository.findById(resumeId) : ");
         Resume resume = resumeJpaRepository.findById(resumeId)
                 .orElseThrow(() -> new Exception404("회원 정보를 찾을 수 없습니다."));
 
+        // todo: boardList를 못 찾음
+        System.out.println("3. List<Board> boardList = boardJpaRepository.findByUserId(sessionUser.getId()) : ");
         List<Board> boardList = boardJpaRepository.findByUserId(sessionUser.getId())
                 .orElseThrow(() -> new Exception404("회원 정보를 찾을 수 없습니다."));
 
         return new OfferResponse.OfferFormDTO(resume, boardList);
+    }
+
+    @Transactional
+    public void createOffer(OfferRequest.OfferDTO reqDTO) {
+        Resume resume = resumeJpaRepository.findById(reqDTO.getResumeId())
+                .orElseThrow(() -> new Exception404("회원 정보를 찾을 수 없습니다."));
+
+        Board board = boardJpaRepository.findById(reqDTO.getBoardId())
+                .orElseThrow(() -> new Exception404("회원 정보를 찾을 수 없습니다."));
+
+        Offer offer = new Offer(resume, board);
+        offerJpaRepository.save(offer);
     }
 }
